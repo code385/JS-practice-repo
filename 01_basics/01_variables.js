87a9345e42f2164b39f1b3d6eaf57578f6dece72
Irfan Khan <codingwithme178@gmail.com>
2026-06-17 08:44:11 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "irfan@gmail.com"
+var accountPassword = "121212"
+ 
+accountCity = "Lahore"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3 ); // code readabililty that should be high
+    //docs read  ecma(tc39.com) and MDN
+// Primitive Data Types
+    //number => range 2 to power 53 
+    // bigint 
+    // string => "" or ''
+    //boolean => true/false
+    //null => standalone value   an also called object 
+    // undefined => not assigned value yet 
+    // symbol => uniqueness 
+
+// Objects 
+   //
+
+
+
+console.log(typeof null);
