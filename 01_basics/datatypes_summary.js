889e4232bd9b5e53c7b921fc7cad7f2e6ffb2d23
Irfan Khan <codingwithme178@gmail.com>
2026-06-19 05:16:13 +0000

    datatype Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,20 @@
+// primitive data types 
+
+// 7 types: string , Number, BigInt, Symbol, boolean, Null, Undefined
+
+//Reference ( Non- Primitive)
+// Arrray, Objects, Functions
+
+const myArr = [ "Irfan", "Khan", "Bahadu"]
+
+const myObject = {
+    name: "irfan",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello Irfan");
+}
+
+console.log(typeof  ["myObject","myArr",  "myFunction"]); 
+console.log(typeof  "myObject","myArr",  "myFunction");
